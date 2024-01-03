9684f90b289f2c1e163650c214e169b9faa5ca5f
Ishan Saha <63413315+IshanSaha05@users.noreply.github.com>
2024-01-03 19:43:29 +0530

    Delete Web-Scrapper-India-Votes directory

--- Web-Scrapper-India-Votes/cmd/main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("Message: Connecting to MongoDb Server.")
-}
--- Web-Scrapper-India-Votes/pkg/config/config.go
+++
@@ -1,4 +0,0 @@
-package config
-
-var ScrapeSiteUrl = "https://www.indiavotes.com/lok-sabha/2014/all-states/16/0"
-var MongoSite = ""
